7771fa64931c30b200c715ed56e3235e948eb154
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-09-10 04:44:07 +0200

    Initial Commit

---
+++ DAZN Media Player Assessment/DAZN_Media_Player_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAZN_Media_Player_AssessmentApp.swift
+//  DAZN Media Player Assessment
+//
+//  Created by user222661 on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DAZN_Media_Player_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
